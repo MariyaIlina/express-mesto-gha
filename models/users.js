ed16a56c9d4f2117c2cff209513f1ddcb8e24cc7
Mariya Ilina <mariy4ilina@yandex.ru>
2023-04-26 19:07:22 +0400

    hotfix/test

--- models/users.js
+++ models/users.js
@@ -1,22 +1,22 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   name: {
     minlength: 2,
     maxlength: 30,
     required: true,
-    type: String
+    type: String,
   },
   about: {
     minlength: 2,
     maxlength: 30,
     required: true,
-    type: String
+    type: String,
   },
   avatar: {
     required: true,
-    type: String
-  }
-})
+    type: String,
+  },
+});
 
-module.exports = mongoose.model('user', userSchema)
+module.exports = mongoose.model('user', userSchema);
